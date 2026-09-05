9c59a9eef39ba64cca4bad78094d981a5cf4304a
Marcus Ng <marcusngwj@msn.com>
2026-09-05 14:39:35 +0800

    fix spacing

--- src/domains/home/components/OverviewSection.tsx
+++ src/domains/home/components/OverviewSection.tsx
@@ -3,20 +3,15 @@ import { TripStatCard } from './TripStatCard';
 
 export const OverviewSection = () => {
   return (
-    <section className="mx-auto max-w-4xl space-y-10">
-      <div className="flex h-dvh items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <div className="grid grid-cols-2 content-center justify-center gap-4">
-            <TripStatCard title="12 Days" description="16 Nov - 27 Nov" />
-            <TripStatCard
-              title="2 Prefectures"
-              description="Osaka -> Okinawa "
-            />
-            <TripStatCard title="3 Tours" description="Klook" />
-            <TripStatCard title="8 Sports" description="Many Adventures" />
-          </div>
-          <Video src="map.mp4" />
+    <section className="mx-auto h-svh max-w-4xl px-4 py-10 md:px-8 md:py-16">
+      <div className="flex h-full flex-col items-center justify-center gap-12 md:gap-16">
+        <div className="grid grid-cols-2 content-center justify-center gap-4">
+          <TripStatCard title="12 Days" description="16 Nov - 27 Nov" />
+          <TripStatCard title="2 Prefectures" description="Osaka -> Okinawa " />
+          <TripStatCard title="3 Tours" description="Klook" />
+          <TripStatCard title="8 Sports" description="Many Adventures" />
         </div>
+        <Video src="map.mp4" />
       </div>
     </section>
   );
