fe3440e32ee7331d5e67c395267699f0f6186e25
Marcus Ng <marcusngwj@msn.com>
2026-09-05 14:48:49 +0800

    fix spacing in card

--- src/domains/home/components/OverviewSection.tsx
+++ src/domains/home/components/OverviewSection.tsx
@@ -5,7 +5,7 @@ export const OverviewSection = () => {
   return (
     <section className="mx-auto h-svh max-w-4xl px-4 py-10 md:px-8 md:py-16">
       <div className="flex h-full flex-col items-center justify-center gap-12 md:gap-16">
-        <div className="grid grid-cols-2 content-center justify-center gap-4">
+        <div className="grid w-full grid-cols-2 content-center justify-center gap-4">
           <TripStatCard title="12 Days" description="16 Nov - 27 Nov" />
           <TripStatCard title="2 Prefectures" description="Osaka -> Okinawa " />
           <TripStatCard title="3 Tours" description="Klook" />
--- src/domains/home/components/TripStatCard.tsx
+++ src/domains/home/components/TripStatCard.tsx
@@ -5,11 +5,11 @@ interface TripStatCardProps {
 
 export const TripStatCard = ({ title, description }: TripStatCardProps) => {
   return (
-    <div className="bg-surface flex flex-col items-center rounded-2xl border border-gray-100 p-5 shadow-sm md:min-w-50 lg:p-15">
-      <div className="text-on-surface text-xl font-bold md:text-2xl">
+    <div className="bg-surface flex flex-col items-center gap-2 rounded-2xl border border-gray-100 p-5 shadow-sm md:min-w-50 md:gap-4 lg:p-15">
+      <div className="text-on-surface text-lg font-bold md:text-3xl lg:text-4xl">
         {title}
       </div>
-      <div className="text-on-muted text-sm">{description}</div>
+      <div className="text-on-muted text-sm md:text-xl">{description}</div>
     </div>
   );
 };
