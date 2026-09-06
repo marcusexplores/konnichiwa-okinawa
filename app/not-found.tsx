66d53563ea957a10cda3ac1a54be33ea10d0a497
Marcus Ng <marcusngwj@msn.com>
2026-09-06 22:16:49 +0800

    add pages

---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import { NotFound } from '@/src/domains/system';
+
+export default function NotFoundPage() {
+  return <NotFound />;
+}
---
+++ src/domains/system/NotFound.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export const NotFound = () => {
+  return <div>Not Found</div>;
+};
