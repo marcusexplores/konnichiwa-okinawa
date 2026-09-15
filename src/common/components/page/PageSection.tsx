4659a42f4729bfc9c967a351d741d38f2f14219f
Marcus Ng <marcusngwj@msn.com>
2026-09-16 01:11:29 +0800

    fix md py

--- src/common/components/page/PageSection.tsx
+++ src/common/components/page/PageSection.tsx
@@ -10,7 +10,7 @@ export const PageSection = ({ children, className }: PageSectionProps) => {
   return (
     <div
       className={cn(
-        'px-4 sm:px-6 md:px-8 lg:px-16',
+        'px-4 sm:px-6 md:px-10 lg:px-16',
         // 'py-5 sm:py-6 md:py-8 lg:py-10',
         'py-10 sm:py-12 md:py-16 lg:py-24',
         // 'pt-10 sm:pt-12 md:pt-16 lg:pt-20',
