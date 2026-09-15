2870ade914cecb96b4a30fbbd29f74d18160f416
Marcus Ng <marcusngwj@msn.com>
2026-09-16 01:09:41 +0800

    update page

--- src/common/components/page/PageSection.tsx
+++ src/common/components/page/PageSection.tsx
@@ -7,5 +7,17 @@ interface PageSectionProps {
 }
 
 export const PageSection = ({ children, className }: PageSectionProps) => {
-  return <div className={cn('px-4', className)}>{children}</div>;
+  return (
+    <div
+      className={cn(
+        'px-4 sm:px-6 md:px-8 lg:px-16',
+        // 'py-5 sm:py-6 md:py-8 lg:py-10',
+        'py-10 sm:py-12 md:py-16 lg:py-24',
+        // 'pt-10 sm:pt-12 md:pt-16 lg:pt-20',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
 };
--- src/domains/budget/Budget.tsx
+++ src/domains/budget/Budget.tsx
@@ -20,9 +20,29 @@ export const Budget = () => {
           centerDefaultPrimaryText={getTotalSumDisplay()}
           centerDefaultSecondaryText="per person"
           classNames={{
-            root: 'h-100 w-full sm:h-125 lg:h-180',
+            root: 'h-100 w-full sm:h-125 xl:h-180',
           }}
         />
+      </PageSection>
+      <PageSection className="h-300 bg-emerald-400 lg:h-100">
+        <p>normal</p>
+        <p className="hidden sm:block">sm</p>
+        <p className="hidden md:block">md</p>
+        <p className="hidden lg:block">lg</p>
+        <p className="hidden xl:block">xl</p>
+        <p className="hidden 2xl:block">2xl</p>
+      </PageSection>
+      {/* <PageSection> */}
+      <PageSection className="bg-amber-200">
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+      </PageSection>
+      {/* <PageSection> */}
+      <PageSection className="bg-blue-800">
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
+        <p>lorem ipsum</p>
       </PageSection>
     </div>
   );
