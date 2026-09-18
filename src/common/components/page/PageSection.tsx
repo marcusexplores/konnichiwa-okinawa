df2ee29c066c59a9a6ced7d1afad3f5bfdbb19c1
Marcus Ng <marcusngwj@msn.com>
2026-09-18 23:40:51 +0800

    fix default section paddings

--- src/common/components/page/PageSection.tsx
+++ src/common/components/page/PageSection.tsx
@@ -10,10 +10,11 @@ export const PageSection = ({ children, className }: PageSectionProps) => {
   return (
     <div
       className={cn(
-        'px-4 sm:px-6 md:px-10 lg:px-16',
-        // 'py-5 sm:py-6 md:py-8 lg:py-10',
-        'py-10 sm:py-12 md:py-16 lg:py-24',
-        // 'pt-10 sm:pt-12 md:pt-16 lg:pt-20',
+        'mx-auto max-w-7xl px-4 py-10',
+        'sm:px-6 sm:py-12',
+        'md:px-10 md:py-16',
+        'lg:px-16 lg:py-24',
+        '2xl:px-0',
         className,
       )}
     >
