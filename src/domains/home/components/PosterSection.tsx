ab8cfa464b79a13da427301495053eff8c3f74f0
Marcus Ng <marcusngwj@msn.com>
2026-09-21 22:29:56 +0800

    add link to posters

--- src/domains/home/components/PosterSection.tsx
+++ src/domains/home/components/PosterSection.tsx
@@ -1,25 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
 import { motion, useScroll, useTransform } from 'motion/react';
 import { cn } from '@/src/common/utilities/classname';
+import { routes } from '@/src/common/utilities/routes';
 import { Poster } from './Poster';
 import day1Poster from '@/public/images/poster-day-1.png';
 import day2Poster from '@/public/images/poster-day-2.png';
 import day11Poster from '@/public/images/poster-day-11.png';
-
-const posters = [
-  { alt: 'Day 1', src: day1Poster },
-  { alt: 'Day 2', src: day2Poster },
-  { alt: 'Day 3', src: day2Poster },
-  { alt: 'Day 4', src: day2Poster },
-  { alt: 'Day 5', src: day2Poster },
-  { alt: 'Day 6', src: day2Poster },
-  { alt: 'Day 7', src: day2Poster },
-  { alt: 'Day 8', src: day2Poster },
-  { alt: 'Day 9', src: day2Poster },
-  { alt: 'Day 10', src: day2Poster },
-  { alt: 'Day 11', src: day11Poster },
-  { alt: 'Day 12', src: day11Poster },
-];
 
 export const PosterSection = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -58,12 +45,30 @@ export const PosterSection = () => {
           style={{ x }}
         >
           {posters.map((item) => (
-            <div key={item.alt} className="w-80 shrink-0 sm:w-120 md:w-150">
-              <Poster src={item.src} alt={item.alt} />
-            </div>
+            <Link key={item.alt} href={item.link}>
+              <div className="w-80 shrink-0 sm:w-120 md:w-150">
+                <Poster src={item.src} alt={item.alt} />
+              </div>
+            </Link>
           ))}
         </motion.div>
       </div>
     </section>
   );
 };
+
+const posters = [
+  { alt: 'Day 0', src: day1Poster, link: routes.itinerary.day0 },
+  { alt: 'Day 1', src: day1Poster, link: routes.itinerary.day1 },
+  { alt: 'Day 2', src: day2Poster, link: routes.itinerary.day2 },
+  { alt: 'Day 3', src: day2Poster, link: routes.itinerary.day3 },
+  { alt: 'Day 4', src: day2Poster, link: routes.itinerary.day4 },
+  { alt: 'Day 5', src: day2Poster, link: routes.itinerary.day5 },
+  { alt: 'Day 6', src: day2Poster, link: routes.itinerary.day6 },
+  { alt: 'Day 7', src: day2Poster, link: routes.itinerary.day7 },
+  { alt: 'Day 8', src: day2Poster, link: routes.itinerary.day8 },
+  { alt: 'Day 9', src: day2Poster, link: routes.itinerary.day9 },
+  { alt: 'Day 10', src: day2Poster, link: routes.itinerary.day10 },
+  { alt: 'Day 11', src: day11Poster, link: routes.itinerary.day11 },
+  { alt: 'Day 12', src: day11Poster, link: routes.itinerary.day12 },
+];
